10a01feba5420c703790ec2acdb524ae6e4cae6f
temp-git-paper <yumyumm24@naver.com>
2025-03-21 21:22:52 +0900

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -25,22 +25,31 @@ def preprocess(dir_name):
             os.makedirs(path)
             print(f"📁 Created missing folder: {folder}")
 
-    save_eml_to_txt(os.path.join(dir_name, "emls"), text_output_dir)
-    save_excel_rows_to_txt(os.path.join(dir_name, "textmessage"), text_output_dir)
-    split_messagingapp_files(os.path.join(dir_name, "messagingapp"), text_output_dir)
+    already_precessed = False
+    for name in os.listdir(dir_name):
+        full_path = os.path.join(dir_name, name)
+        if os.path.isdir(full_path) and "A2P-classified" in name:
+            already_precessed = True
+    if not already_precessed:
+        save_eml_to_txt(os.path.join(dir_name, "emls"), text_output_dir)
+        save_excel_rows_to_txt(os.path.join(dir_name, "textmessage"), text_output_dir)
+        split_messagingapp_files(os.path.join(dir_name, "messagingapp"), text_output_dir)
 
-    # Step 2: Classifying A2P messages
-    print("Step 2: Classifying A2P messages...")
-    a2p_classified_dir = os.path.join(dir_name, "A2P-classified-text")
-    process_txt_files([text_output_dir], a2p_classified_dir)
+        # Step 2: Classifying A2P messages
+        print("Step 2: Classifying A2P messages...")
+        a2p_classified_dir = os.path.join(dir_name, "A2P-classified-text")
+        process_txt_files([text_output_dir], a2p_classified_dir)
 
-    html_output_dir = os.path.join(dir_name, "A2P-classified-html")
-    convert_text_to_html(a2p_classified_dir, html_output_dir)
+        html_output_dir = os.path.join(dir_name, "A2P-classified-html")
+        convert_text_to_html(a2p_classified_dir, html_output_dir)
 
-    # Step 3: Extracting keywords
-    print("Step 3: Extracting keywords from A2P messages...")
-    keyword_output_dir = os.path.join(dir_name, "A2P-classified-json")
-    extract_keywords([a2p_classified_dir], keyword_output_dir)
+        # Step 3: Extracting keywords
+        print("Step 3: Extracting keywords from A2P messages...")
+        keyword_output_dir = os.path.join(dir_name, "A2P-classified-json")
+        extract_keywords([a2p_classified_dir], keyword_output_dir)
+    else:
+        html_output_dir = os.path.join(dir_name, "A2P-classified-html")
+        keyword_output_dir = os.path.join(dir_name, "A2P-classified-json")
 
     print("Processing completed.")
 
